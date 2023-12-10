5f072a26542879a91e5bd7ef5c152f9332d6c05e
xrun1 <126838916+xrun1@users.noreply.github.com>
2023-12-10 15:05:46 -0400

    Add HLS quality selector to video player

--- update-static.sh
+++ update-static.sh
@@ -3,7 +3,9 @@ set -e
 
 get() { wget -nv "$1" -O "$(dirname "$0")/xrtube/static/${1##*/}" & }
 get https://unpkg.com/htmx.org@^1.9.9/dist/htmx.min.js
+get https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css
 get https://unpkg.com/video.js@^8.6.1/dist/video.min.js
 get https://unpkg.com/video.js@^8.6.1/dist/video-js.min.css
-get https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css
+get https://unpkg.com/videojs-contrib-quality-levels@4.0.0/dist/videojs-contrib-quality-levels.js
+get https://unpkg.com/jb-videojs-hls-quality-selector@2.0.2/dist/jb-videojs-hls-quality-selector.js
 wait
