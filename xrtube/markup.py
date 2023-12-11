2c7096deb9a143f751e0985792e148294e5a65b6
xrun1 <126838916+xrun1@users.noreply.github.com>
2023-12-11 14:45:12 -0400

    YT markup: fix URLs containing & getting broken by html.escape

--- xrtube/markup.py
+++ xrtube/markup.py
@@ -1,5 +1,6 @@
 import html
 import re
+from uuid import UUID, uuid4
 
 
 def build_youtube_markup_regex(symbol: str) -> re.Pattern:
@@ -20,20 +21,32 @@ YT_MARKUP = {
 
 
 def yt_to_html(text: str) -> str:
-    def format_hashtag(match: re.Match) -> str:
-        return f"""<a
+    parts: dict[UUID, str] = {}
+
+    def prepare_url(match: re.Match) -> str:
+        parts[(id := uuid4())] = f'<a href="{match[1]}">{match[1]}</a>'
+        return str(id)
+
+    def prepare_hashtag(match: re.Match) -> str:
+        parts[(id := uuid4())] = f"""<a
             href="/hashtag/{match[1]}"
             hx-get="/hashtag/{match[1]}"
             hx-select="#explore-column"
             hx-target="#explore-column"
             hx-push-url=true
         >#{match[1]}</a>"""
+        return str(id)
 
-    text = html.escape(text, quote=False)
-
+    # The URLs/etc can contain characters like & which html.escape would break,
+    # which is why do their replacements in two steps
+    text = URL.sub(prepare_url, text)
+    text = HASHTAGS.sub(prepare_hashtag, text)
     for tag, regex in YT_MARKUP.items():
         text = regex.sub(rf"<{tag}>\1</{tag}>", text)
 
-    text = URL.sub(r'<a href="\1">\1</a>', text)
-    text = HASHTAGS.sub(format_hashtag, text)
+    text = html.escape(text, quote=False)
+    # Only after escaping normal text, we can inject our custom HTML
+    for uuid, replacement in parts.items():
+        text = text.replace(str(uuid), replacement)
+
     return text
