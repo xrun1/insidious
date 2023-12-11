b817f718586adfafa46a1994099a69c4312b5a0d
xrun1 <126838916+xrun1@users.noreply.github.com>
2023-12-11 15:00:52 -0400

    Replace YT links in markup with links to our own server

--- xrtube/markup.py
+++ xrtube/markup.py
@@ -2,6 +2,7 @@ import html
 import re
 from urllib.parse import unquote
 from uuid import UUID, uuid4
+from fastapi.datastructures import URL
 
 
 def build_youtube_markup_regex(symbol: str) -> re.Pattern:
@@ -9,12 +10,12 @@ def build_youtube_markup_regex(symbol: str) -> re.Pattern:
     return re.compile(rf"(?:^|(?<=\s)){sym}(.*?){sym}(?=\s|$)")
 
 
-URL = re.compile(
+URL_RE = re.compile(
     r"(https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}"
     r"\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))"
 )
-HASHTAGS = re.compile(r"(?:^|(?<=\W))#(\w+)")
-YT_MARKUP = {
+HASHTAG_RE = re.compile(r"(?:^|(?<=\W))#(\w+)")
+MARKUP_RE = {
     "strong": build_youtube_markup_regex("*"),
     "em": build_youtube_markup_regex("_"),
     "del": build_youtube_markup_regex("-"),
@@ -24,9 +25,15 @@ YT_MARKUP = {
 def yt_to_html(text: str) -> str:
     parts: dict[UUID, str] = {}
 
-    def prepare_url(match: re.Match) -> str:
-        pretty = html.escape(unquote(match[1]), quote=False)
-        parts[(id := uuid4())] = f'<a href="{match[1]}">{pretty}</a>'
+    def prepare_url(match: re.Match[str]) -> str:
+        url = URL(match[1])
+        pretty = html.escape(unquote(str(url)), quote=False)
+        host = (url.hostname or "").removeprefix("www.")
+
+        if host in ("youtube.com", "youtube-nocookie.com", "youtu.be"):
+            url = url.replace(scheme="", hostname="")
+
+        parts[(id := uuid4())] = f'<a href="{url}">{pretty}</a>'
         return str(id)
 
     def prepare_hashtag(match: re.Match) -> str:
@@ -41,9 +48,9 @@ def yt_to_html(text: str) -> str:
 
     # The URLs/etc can contain characters like & which html.escape would break,
     # which is why do their replacements in two steps
-    text = URL.sub(prepare_url, text)
-    text = HASHTAGS.sub(prepare_hashtag, text)
-    for tag, regex in YT_MARKUP.items():
+    text = URL_RE.sub(prepare_url, text)
+    text = HASHTAG_RE.sub(prepare_hashtag, text)
+    for tag, regex in MARKUP_RE.items():
         text = regex.sub(rf"<{tag}>\1</{tag}>", text)
 
     text = html.escape(text, quote=False)
