305373bf2a53c32685dfefc50614899fc5336f27
xrun1 <126838916+xrun1@users.noreply.github.com>
2024-03-30 16:17:08 -0400

    Use hls.js fork to allow level auto-switch even between codecs
    This reverts commit e0bbdbb41e184803141d7d1b7fccde8356850c93.

--- update-static.sh
+++ update-static.sh
@@ -21,6 +21,7 @@ get hls-video-element@1.1.3/+esm
 # hls-video-element dependencies
 get custom-media-element@1.2.3/+esm
 get media-tracks@0.3.0/+esm
-get hls.js@1.5.4/dist/hls.mjs/+esm
+# HACK: using build from xrun1/hls.js fork instead
+# get hls.js@1.5.4/dist/hls.mjs/+esm
 
 wait
