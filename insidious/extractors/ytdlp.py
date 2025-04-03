fe2601613b1082a52d5ebbc3861d4dbee83d8322
xrun1 <126838916+xrun1@users.noreply.github.com>
2025-04-03 16:31:42 +0200

    Switch back to ios yt-dlp client
    Seems like web_safari no longer exists and ios is fixed, so

--- insidious/extractors/ytdlp.py
+++ insidious/extractors/ytdlp.py
@@ -346,7 +346,7 @@ class YtdlpClient(YoutubeClient):
             "compat_opts": ["no-youtube-unavailable-videos"],
             "extractor_args": {
                 # This client has the HLS manifests, no need for others
-                "youtube": {"player_client": ["web_safari"]},
+                "youtube": {"player_client": ["ios"]},
                 # Retrieve upload dates in flat playlists
                 "youtubetab": {"approximate_date": ["timestamp"]},
             },
